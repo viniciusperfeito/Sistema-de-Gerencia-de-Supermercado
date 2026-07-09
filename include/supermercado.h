967b440e22eff20bb575a26c21f1ea7cfd14029f
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-09 20:29:27 -0300

    Criacao do prototipo das funcoes e do struct

--- include/supermercado.h
+++ include/supermercado.h
@@ -1,1 +1,16 @@
-#include <stdio.h>
+#ifndef SUPERMERCADO_H
+#define SUPERMERCADO_H
+
+#define MAX_PRODUTOS 3000
+
+typedef struct {
+    char nome[50];
+    int quantidade;
+    float preco;
+    float faturamento;
+    int situacao;
+} Produto;
+
+void funcao_cadastra_produtos(Produto produtos[], int *total);
+
+#endif
