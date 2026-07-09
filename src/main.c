ae48dbf8b862df7a55841a442de662d2a51bacc4
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-09 20:01:19 -0300

    Cria main com estrutura do switch case

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#ifdef SUPERMERCADO_H
+#define SUPERMERCADO_H
+#include "/home/viniciusperfeito/Documents/Repositorios/Sistema-de-Supermercado/include/supermercado.h"
+
+int main (void) {
+
+}
+
+
+#endif
