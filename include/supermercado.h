ee518431637da96c213f91d7d0e4c630e6bfc2c2
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-11 17:19:04 -0300

    Criacao do prototipo das funcoes e suas dependencias

--- include/supermercado.h
+++ include/supermercado.h
@@ -2,7 +2,9 @@
 #define SUPERMERCADO_H
 
 #define MAX_PRODUTOS 3000
+#define MAX_VENDAS 50000
 
+// Estrutura para representar um produto
 typedef struct {
     char nome[50];
     int quantidade;
@@ -10,7 +12,20 @@ typedef struct {
     float faturamento;
     int situacao;
 } Produto;
+// Estrutura de data de vendas
+typedef struct {
+    char data[11];  // Exemplo: "dd/mm/yyyy"
+    float valor;    // Valor total da venda
+    char nome[50];  // Nome do produto vendido
+    int quantidade; // Quantidade vendida
+} Venda;
 
+// Declaração das funções
 void funcao_cadastra_produtos(Produto produtos[], int *total);
+void funcao_controle_estoque(Produto produtos[], int total);
+void funcao_imprime_produtos(Produto produtos[], int total);
+void funcao_busca_produtos(Produto produtos[], int total, char nome[]);
+void funcao_venda_produtos(Produto produtos[], Venda vendas[], int total, int *total_vendas);
+void funcao_faturamento(Venda vendas[], int total_vendas);
 
 #endif
