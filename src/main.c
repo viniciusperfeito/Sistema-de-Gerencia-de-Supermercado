2892443e814264ae23bc4b11c94137629712c1f7
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-11 17:20:20 -0300

    Declaracao das variaveis de vendas e produtos e suas dependencias

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include "../include/supermercado.h"
 
 int main(void) {
-    // Produto vetor e total
-    Produto produtos[MAX_PRODUTOS];
-    int total = 0;
+    Produto produtos[MAX_PRODUTOS];     // Produto vetor e total
+    Venda vendas[MAX_VENDAS];         // Venda vetor e total
+    int total = 0;      // Total de produtos cadastrados
+    int total_vendas = 0;   // Total de vendas realizadas
     printf("Sistema de Supermercado\n");
+
 }
