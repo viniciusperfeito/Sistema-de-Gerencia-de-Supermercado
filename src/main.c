b50ba07810a354017dae8b85f98a3b0f01c1a176
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 14:11:28 -0300

    Crição do menu com switch case

--- src/main.c
+++ src/main.c
@@ -6,6 +6,37 @@ int main(void) {
     Venda vendas[MAX_VENDAS];         // Venda vetor e total
     int total = 0;      // Total de produtos cadastrados
     int total_vendas = 0;   // Total de vendas realizadas
-    printf("Sistema de Supermercado\n");
+    printf("Sistema de Supermercado\n"); // Titulo do Menu
+    int opcao; // Variavel de opcao do usuario
+    printf("Digite a opção desejada:\n");
+    printf("1 - Cadastrar produto\n");
+    printf("2 - Controle de Estoque\n");
+    printf("3 - Mostrar nome dos produtos\n");
+    printf("4 - Exiba os dados dos produtos\n");
+    printf("5 - Realizar venda\n");
+    printf("6 - Exibir relatório de vendas\n");
+    scanf("%d", &opcao);
+    // Switch para executar a opcao escolhida pelo usuario
+    switch (opcao) {
+        case 1:
 
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        default:
+            printf("Opção inválida\n");
+            break;
+    }
 }
