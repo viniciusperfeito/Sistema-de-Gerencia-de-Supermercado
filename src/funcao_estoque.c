c3e622753a8cc3a4b7ee0ff871f97557079e5e31
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 14:24:55 -0300

    Alteracao do total no for

--- src/funcao_estoque.c
+++ src/funcao_estoque.c
@@ -3,7 +3,7 @@
 void funcao_controle_estoque(Produto produtos[], int total) {
     printf("Controle de Estoque\n");
     printf("Produtos com estoque baixo:\n");
-    for (int i = 0; i < produtos[i].quantidade; i++) {
+    for (int i = 0; i < total; i++) {
         if (produtos[i].quantidade <= 5) {
             printf("%s - Quantidade: %d\n", produtos[i].nome, produtos[i].quantidade);
         }
