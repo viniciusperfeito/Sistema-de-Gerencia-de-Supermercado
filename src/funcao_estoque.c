529d7c41f75cdac8b87512a4b820b3523a92fa7c
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 16:58:17 -0300

    Puts() dentro do if

--- src/funcao_estoque.c
+++ src/funcao_estoque.c
@@ -6,7 +6,7 @@ void funcao_controle_estoque(Produto produtos[], int total) {
     for (int i = 0; i < total; i++) {
         if (produtos[i].quantidade <= 5) {
             printf("%s - Quantidade: %d\n", produtos[i].nome, produtos[i].quantidade);
+            puts("");
         }
-        puts("");
     }
 }
