40d056e580296989cc38d0fff9a65bdaa14a69a6
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 14:30:29 -0300

    Criacao e configuracao parcial da funcao_venda

---
+++ src/funcao_venda.c
@@ -0,0 +1,9 @@
+#include "/home/viniciusperfeito/Documents/Repositorios/Sistema-de-Supermercado/include/supermercado.h"
+
+void funcao_venda_produtos(Produto produtos[], Venda vendas[], int total, int *total_vendas) {
+    printf("Venda de Produtos\n");
+
+    for (int i = 0; i < total; i++) {
+        printf("");
+    }
+}
