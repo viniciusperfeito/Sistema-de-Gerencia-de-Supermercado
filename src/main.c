a3b92786f7d0974fd8e0bd20120f73b2f5a642ad
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 16:34:06 -0300

    Add switch case dentro de um loop

--- src/main.c
+++ src/main.c
@@ -7,35 +7,37 @@ int main(void) {
     int total_vendas = 0;   // Total de vendas realizadas
     printf("Sistema de Supermercado\n"); // Titulo do Menu
     int opcao; // Variavel de opcao do usuario
-    printf("Digite a opção desejada:\n");
-    printf("1 - Cadastrar produto\n");
-    printf("2 - Controle de Estoque\n");
-    printf("3 - Mostrar nome dos produtos\n");
-    printf("4 - Exiba os dados dos produtos\n");
-    printf("5 - Realizar venda\n");
-    printf("6 - Exibir relatório de vendas\n");
-    scanf("%d", &opcao);
-    // Switch para executar a opcao escolhida pelo usuario
-    switch (opcao) {
-        case 1:
+    while (opcao == 0) {
+        printf("Digite a opção desejada:\n");
+        printf("1 - Cadastrar produto\n");
+        printf("2 - Controle de Estoque\n");
+        printf("3 - Mostrar nome dos produtos\n");
+        printf("4 - Exiba os dados dos produtos\n");
+        printf("5 - Realizar venda\n");
+        printf("6 - Exibir relatório de vendas\n");
+        scanf("%d", &opcao);
+        // Switch para executar a opcao escolhida pelo usuario
+        switch (opcao) {
+            case 1:
 
-        case 2:
-            funcao_controle_estoque(produtos, total);
-            break;
-        case 3:
+            case 2:
+                funcao_controle_estoque(produtos, total);
+                break;
+            case 3:
 
-            break;
-        case 4:
+                break;
+            case 4:
 
-            break;
-        case 5:
+                break;
+            case 5:
 
-            break;
-        case 6:
-            funcao_faturamento(vendas, total_vendas);
-            break;
-        default:
-            printf("Opção inválida\n");
-            break;
+                break;
+            case 6:
+                funcao_faturamento(vendas, total_vendas);
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
+        }
     }
 }
