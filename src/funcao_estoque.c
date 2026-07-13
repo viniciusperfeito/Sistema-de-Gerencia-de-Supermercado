3c9323e60ef3a4f4b5cbeb751f35cdf40f0e0d72
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 14:16:43 -0300

    Criação do arquivo da funcao_estoque

---
+++ src/funcao_estoque.c
@@ -0,0 +1,6 @@
+#include "/home/viniciusperfeito/Documents/Repositorios/Sistema-de-Supermercado/include/supermercado.h"
+
+void funcao_controle_estoque(Produto produtos[], int total) {
+    printf("Controle de Estoque\n");
+
+}
