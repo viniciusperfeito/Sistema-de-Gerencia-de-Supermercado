673e11aedd4e6a95e4e0334551a78de044a15f49
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-13 14:59:36 -0300

    Deletamento da opcao semanal e ajuste no faturamento toal

--- src/funcao_faturamento.c
+++ src/funcao_faturamento.c
@@ -4,16 +4,15 @@ void funcao_faturamento(Venda vendas[], int total_vendas) {
     printf("Faturamento\n");
     int opcao;
     int data[3];
-    int faturamento_total = 0;
+    double faturamento_total = 0;
     // Pergunta a data de hoje para filtrar as vendas
     printf("Digite a data de hoje (dd/mm/yyyy): ");
     scanf("%d/%d/%d", &data[0], &data[1], &data[2]);
     // Pergunta a opção de período de faturamento
     printf("Você deseja ter acesso a qual período de faturamento?\n");
     printf("1. Diário\n");
-    printf("2. Semanal\n");
-    printf("3. Mensal\n");
-    printf("4. Anual\n");
+    printf("2. Mensal\n");
+    printf("3. Anual\n");
     scanf("%d", &opcao);
     switch (opcao) {
         case 1:
@@ -24,22 +23,10 @@ void funcao_faturamento(Venda vendas[], int total_vendas) {
                     printf("Venda %d: R$ %.2f\n", i + 1, vendas[i].valor);
                     faturamento_total += vendas[i].valor;
                 }
-
             }
+            printf("Faturamento total: R$ %.2f\n", faturamento_total);
             break;
         case 2:
-            printf("Semanal\n");
-            for (int i = 0; i < total_vendas; i++) {
-                // Cria uma variável para armazenar o número da semana
-                int semanal = vendas[i].data[1] / 7;
-                // Verifica se a venda foi realizada na data informada
-                if (semanal == data[1] / 7 && vendas[i].data[2] == data[2]) {
-                    printf("Venda %d: R$ %.2f\n", i + 1, vendas[i].valor);
-                    faturamento_total += vendas[i].valor;
-                }
-            }
-            break;
-        case 3:
             printf("Mensal\n");
             for (int i = 0; i < total_vendas; i++) {
                 // Verifica se a venda foi realizada na data informada
@@ -48,8 +35,9 @@ void funcao_faturamento(Venda vendas[], int total_vendas) {
                     faturamento_total += vendas[i].valor;
                 }
             }
+            printf("Faturamento total: R$ %.2f\n", faturamento_total);
             break;
-        case 4:
+        case 3:
             printf("Anual\n");
             for (int i = 0; i < total_vendas; i++) {
                 // Verifica se a venda foi realizada na data informada
@@ -58,6 +46,7 @@ void funcao_faturamento(Venda vendas[], int total_vendas) {
                     faturamento_total += vendas[i].valor;
                 }
             }
+            printf("Faturamento total: R$ %.2f\n", faturamento_total);
             break;
         default:
             printf("Opção inválida\n");
