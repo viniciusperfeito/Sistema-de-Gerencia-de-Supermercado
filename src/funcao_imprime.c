6e36c3fbebf212da39a7ec5820cf9906ef100329
Vinicius Perfeito <viniciusperfeito1000@gmail.com>
2026-07-15 21:33:14 -0300

    Troca da situacao do produto para 0 como reposicao e 1 como situacao normal

--- src/funcao_imprime.c
+++ src/funcao_imprime.c
@@ -16,7 +16,7 @@ void funcao_imprime_produtos(Produto produtos[], int total, int completo) {
             // Imprime nome e demais dados do produto
             printf("ID: %d | Nome: %s | Quantidade: %d | Preco: R$ %.2f | Faturamento: R$ %.2f | Situacao: %s\n",
                    i, produtos[i].nome, produtos[i].quantidade, produtos[i].preco,
-                   produtos[i].faturamento, (produtos[i].situacao == 1) ? "REPOSICAO" : "OK");
+                   produtos[i].faturamento, (produtos[i].situacao == 0) ? "REPOSICAO" : "OK");
         } else {
             // Imprime somente o nome do produto
             printf("ID: %d | Nome: %s\n", i, produtos[i].nome);
